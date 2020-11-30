fe563b911a2788074b599f72de5a0ba8276cb0a9
Chloé Allgeyer <chloe.allgeyer@gmail.com>
2020-11-30 09:20:54 +0100

    final version 1.3

--- routes/sauces.js
+++ routes/sauces.js
@@ -5,9 +5,6 @@ const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-// /!\ A SUPPRIMER AVANT MISE EN PROD DEV ONLY
-router.get('/dev', saucesCtrl.getSauces);
-
 router.get('/', auth, saucesCtrl.getSauces);
 router.get('/:id', auth, saucesCtrl.getSauceById);
 router.post('/', auth, multer, saucesCtrl.createSauce);
